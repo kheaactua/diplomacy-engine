1ace062154dcfbe46909ab03395dd57001e07472
Gray O'Byrne <obyrne.gray@gmail.com>
2015-05-24 12:13:32 -0400

    Adding some controller for future commands. Call to games source works.

--- app/app.js
+++ app/app.js
@@ -6,9 +6,38 @@
 		this.games = "Hello, these are the games";
 		var gamesCtrl = this;
 		
-		$http.get('http://diplomacy.asilika.com:9494/api/rest/games').success(function(data){
-		  gamesCtrl.games = data;
+		$http.get('http://diplomacy.asilika.com:9494/api/rest/games').success(function(response){
+		  gamesCtrl.games = response.data.data;
 		});
 	}]);
-  
+	
+	app.controller('EmpiresController', ['$http', function($http){
+   
+		this.empires = "Hello, these are the empires";
+		var empiresCtrl = this;
+		this.match = {};
+		
+		$http.get('http://diplomacy.asilika.com:9494/api/rest/matches/108').success(function(data){
+		  empiresCtrl.empires = data;
+		});
+	}]);
+ 
+//	app.controller('MatchesController', ['$http', function($http){
+//  
+//		this.matches = "Hello, these are the matches";
+//		var matchesCtrl = this;
+//		
+//		$http.get('http://diplomacy.asilika.com:9494/api/rest/matches').success(function(data){
+//		  matchesCtrl.matches = data;
+//		});
+//	}]);
+	
+	app.controller('UnitController', ['$http', function($http){
+   
+		this.territories = [{unitType:'army', territory: 'canada'},{unitType:'fleet', territory: 'greenland'},{unitType:'none', territory: 'cuba'}];
+		
+		//$http.get('http://diplomacy.asilika.com:9494/api/rest/matches').success(function(data){
+		 // 
+		//});
+	}]);
 })();
