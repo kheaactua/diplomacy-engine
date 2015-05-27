b8c730f84eb0726fb6c67d0af940ce6b415aa5fe
Matthew Russell <matthew.g.russell@gmail.com>
2015-05-27 00:54:44 -0400

    Whitespace

--- app/app.js
+++ app/app.js
@@ -1,64 +1,65 @@
 (function(){
 	var app = angular.module('diplomacy', []);
-	
+
 	var server = 'http://diplomacy2.asilika.com:9494';
-	
+
 	app.controller('GamesController', ['$http', function($http){
-   
+
 		this.games = "Hello, these are the games";
 		var gamesCtrl = this;
-		
+
 		$http.get( server + '/api/rest/games').success(function(response){
 		  gamesCtrl.games = response.data.data;
 		});
 	}]);
- 
+
 	app.controller('MatchesController', ['$http', '$scope', '$window', function($http, $scope, $window){
-  
+
 		$scope.action = 'hold';
-  
+
 		this.matches = "Hello, these are the matches";
 		var matchesCtrl = this;
-		
+
 		$http.get( server + '/api/rest/matches').success(function(response){
 		  matchesCtrl.matches = response.data.data;
 		});
-		
+
 		this.setMatch = function(matchId){
 			this.selectedMatch=this.matches[matchId];
-			
+
 			//Get empires for this match
 			$http.get( server + '/api/rest/matches/'+matchId).success(function(response){
 				matchesCtrl.empires = response.data.data;
 			});
 		}
-		
+
 		this.setEmpire = function(matchId, empireId){
 			this.selectedEmpire=this.empires[empireId];
-			
+
 			//Get territories for this match
 			$http.get( server + '/api/rest/matches/'+matchId+'/empires/'+empireId+'/territories?include_neighbours=1').success(function(response){
 				matchesCtrl.territories = response.data.data;
 			});
 		}
-		
+
 		this.sendOrders = function(matchId, empireId, ordersText){
 			var req = server + '/api/rest/matches/'+matchId+'/empires/'+empireId+'/orders?order_str='+escape(ordersText);
 			console.debug(req);
-			
+
 			$http.get(req).success(function(response){
 				console.debug(response);
 			});
 		}
 	}]);
-	
+
 	app.directive("neighbourTerritories", function(){
 		return {
 		  restrict: 'E',
 		  templateUrl: 'neighbour-territories.html'
 		};
-	
+
 	});
-	
 
 })();
+
+// vim: noet sts=0 sw=4 ts=4 :
