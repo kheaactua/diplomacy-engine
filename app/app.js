f334a42b5f53332a0812d2e94439b34e379f5b49
Matthew Russell <matthew.g.russell@gmail.com>
2015-05-28 02:37:20 -0400

    Fixed up some bugs in Gray's code, removed the need for JQuery

--- app/app.js
+++ app/app.js
@@ -1,19 +1,14 @@
 (function(){
 	var app = angular.module('diplomacy', []);
 
-	var server = 'http://diplomacy3.asilika.com';
+	//var server = 'http://diplomacy3.asilika.com';
+	var server = 'http://diplomacy.asilika.com:9494';
 
-	app.controller('GamesController', ['$http', function($http){
+	app.controller('MatchesController', ['$http', '$scope', '$window', 'TerritoryController', 'Territory', function($http, $scope, $window, TerritoryController, territory){
 
-		this.games = "Hello, these are the games";
-		var gamesCtrl = this;
+		territoryController = TerritoryController.getTerritoryController();
 
-		$http.get( server + '/api/rest/games').success(function(response){
-		  gamesCtrl.games = response.data.data;
-		});
-	}]);
-
-	app.controller('MatchesController', ['$http', '$scope', '$window', function($http, $scope, $window){
+		$scope.territories = [];
 
 		$scope.action = 'hold';
 
@@ -35,21 +30,45 @@
 
 			//Get territories for this match
 			$http.get( server + '/api/rest/matches/'+matchId+'/empires/'+empireId+'/territories?include_neighbours=1').success(function(response){
-				$scope.territories = response.data.data;
+				angular.forEach(response.data.data, function (val, idx) {
+					var t = territoryController.push(val.territory);
+					$scope.territories.push(t);
+				});
+				angular.forEach(response.data.data, function (val, idx) {
+					var t = territoryController.get(val.territory.territoryId);
+					t.setUnit(val.unit);
+					t.loadNeighbours(val.neighbours);
+				});
 			});
 		}
 
-		/*
-		$scope.sendOrders = function(matchId, empireId, order, territory, destination, source){
-			var ordersText = order + " " + territory + " " + destination + " " + source;
+		/**
+		 * Iterates through all the orders in the form, and submits them one-by-one
+		 */
+		$scope.sendAllOrders = function() {
+			angular.forEach($scope.orders, function(val) {
+				sendOrder($scope.matchId, $scope.empireId, val);
+			});
+		}
+
+		sendOrder = function(matchId, empireId, order, orderText){
 			var req = server + '/api/rest/matches/'+matchId+'/empires/'+empireId+'/orders?order_str='+escape(ordersText);
-			console.debug(req);
 
 			$http.get(req).success(function(response){
 				console.debug(response);
 			});
 		}
-		*/
+
+		$scope.orders = [];
+		$scope.writeMoveOrder = function(territoryId) {
+			$scope.orders.push('MOVE "' + territoryController.get(parseInt(territoryId)).name + '"');
+		}
+		$scope.writeSupportOrder = function(sourceId, destId) {
+			$scope.orders.push('SUPPORT ' + territoryController.get(parseInt(sourceId)).name + ' "' + + territoryController.get(parseInt(sourceId)).name + '"');
+		}
+		$scope.writeConvoyOrder = function(sourceId, destId) {
+			$scope.orders.push('CONVOY ' + territoryController.get(parseInt(sourceId)).name + ' "' + + territoryController.get(parseInt(sourceId)).name + '"');
+		}
 	}]);
 
 	app.directive("neighbourTerritories", function(){
@@ -57,7 +76,6 @@
 		  restrict: 'E',
 		  templateUrl: 'neighbour-territories.html'
 		};
-
 	});
 
 })();
